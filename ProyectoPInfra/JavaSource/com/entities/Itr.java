dfee806ebc14bb12d194c9ec648501eada1a83ba
viera <viera@192.168.56.1>
2023-06-27 09:42:51 -0300

    adsa

---
+++ ProyectoPInfra/JavaSource/com/entities/Itr.java
@@ -0,0 +1,15 @@
+package com.entities;
+
+import com.enums.EstadoItr;
+
+public class Itr {
+	private long idItr;
+	
+	private String nombre;
+	
+	private String departamento;
+	
+	private String localidad;
+	
+	private EstadoItr estadoItr;
+}
---
+++ ProyectoPInfra/JavaSource/com/entities/Usuario.java
@@ -0,0 +1,34 @@
+package com.entities;
+
+import java.util.Date;
+
+import com.enums.TipoUsuario;
+import com.enums.Verificacion;
+
+public class Usuario {
+	private long idUsuario;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private int documento;
+	
+	private Date fechaNacimiento;
+	
+	private String direccion;
+	
+	private String nombreUsuario;
+	
+	private String contrasenia;
+	
+	private TipoUsuario tipoUsuario;
+	
+	private Verificacion verificacion;
+	
+	private String mail;
+	
+	private String telefono;
+	
+	private 
+}
